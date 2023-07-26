55305e4c27bc5e6572ef102fdc49e4020bdb614f
De1eF <jatfun58@gmail.com>
2023-07-26 23:16:04 +0200

    first commit

---
+++ src/main/java/com/budkevych/uniserviceapi/UniServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.budkevych.uniserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniServiceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniServiceApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/budkevych/uniserviceapi/UniServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budkevych.uniserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniServiceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
